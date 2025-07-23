4a07872576574e24bde05da8bfd544fca92007b8
Nivid Patel <nividpatel18102004@gmail.com>
2025-07-22 23:39:09 -0400

    change db config

--- db_config.py
+++ db_config.py
@@ -1,7 +1,7 @@
 DB_CONFIG = {
     "dbname": "listings_db",
     "user": "postgres",
-    "password": "pass123",
+    "password": "ENTER_YOUR_PASSWORD",
     "host": "localhost",
     "port": 5432
 }
